d7605561ac4bf28605d66a2d26c265e8d160df92
Sergey <neychychyen@mail.ru>
2024-11-15 09:58:40 +0500

    Переписан js в удобном декомпозированном формате, добавлена фунция отключения скрола при изменении масштаба

--- js.js
+++ js.js
@@ -1,21 +1,28 @@
+// ПРЕДНАСТРОЙКИ переменные
 let CurrentScroll = getCurrentScroll()
+
+
+let CurrentTransition = get_transtion()
+
+
+// ПРЕДНАСТРОЙКИ функции
 return_scroll(getCurrentScroll())
 
+// ФУНКЦИИ
 
-function getCurrentScroll() {
+
+function getCurrentScroll() { // Узнать текущий скролл по Y
         return window.scrollY || document.documentElement.scrollTop || document.body.scrollTop || 0;
       }
 
-function return_scroll(scrollPosition) {
-  document.querySelector('#scroly').innerText = 'Текущий скролл: ' + scrollPosition;
-      
-}
 
-function removeLastChar(str) {
+
+
+function removeLastChar(str) {// Нужен для get_transtion
   return str.slice(0, -1);
 }
 
-function get_transtion(){
+function get_transtion(){ // Выудить базовую задержку анимацции из css
   let result = getComputedStyle(document.documentElement).getPropertyValue('--transition_time');
   result = removeLastChar(result);
   return parseFloat(result)*1000;
@@ -23,11 +30,10 @@ function get_transtion(){
 
 
 
-window.addEventListener('scroll', function() {
+function event_scroll(){// Что происходит при скроле, нужен для window.addEventListener('scroll'
+
 
     let NewScroll = getCurrentScroll()
-
-    return_scroll(getCurrentScroll())
 
     const mainMenu = document.querySelector('.main_menu');
     
@@ -48,4 +54,63 @@ window.addEventListener('scroll', function() {
       mainMenu.classList.remove('shrink');
     }
     CurrentScroll = NewScroll
-  });
+
+
+}
+
+
+
+
+
+
+function get_transtion(){
+  let result = getComputedStyle(document.documentElement).getPropertyValue('--transition_time');
+  result = removeLastChar(result);
+  return parseFloat(result)*1000;
+}
+
+
+function disableScroll() {
+  document.body.style.overflow = 'hidden'; // Отключаем прокрутку
+}
+function enableScroll() {
+  document.body.style.overflow = ''; // Восстанавливаем прокрутку
+}
+function handleResize(){
+        disableScroll();
+
+  // Восстанавливаем прокрутку через 1 секунду (1000 миллисекунд)
+  setTimeout(enableScroll, CurrentTransition);
+}
+
+
+// Используются в фронтенде
+
+function return_resize() {
+  const width = window.innerWidth;
+  document.querySelector('#scale').innerText = 'Текущий scale: ' + window.innerWidth;
+}
+
+function return_scroll(scrollPosition) {
+  console.log(scrollPosition)
+  document.querySelector('#scroly').innerText = 'Текущий скролл: ' + scrollPosition;
+      
+}
+
+
+
+// Ивенты
+
+
+window.addEventListener('scroll', function(){
+        event_scroll();
+        return_scroll(CurrentScroll);
+
+} );
+
+
+window.addEventListener('resize', function(){
+        handleResize();
+        return_resize();
+
+} );
